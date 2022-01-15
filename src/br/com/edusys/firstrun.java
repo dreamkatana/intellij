38183abe3e70603521923e48fa439f04841095eb
Santos <joaoedu@gmail.com>
2022-01-15 06:56:03 -0300

    first commit

---
+++ src/br/com/edusys/firstrun.java
@@ -0,0 +1,12 @@
+package br.com.edusys;
+
+import br.com.edusys.model.Gato;
+
+public class firstrun {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+
+}
