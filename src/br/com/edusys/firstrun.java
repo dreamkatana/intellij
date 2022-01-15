1f41b667bb4ae48359d66d436def293d0e49d82b
Santos <joaoedu@gmail.com>
2022-01-15 15:00:19 -0300

    Cria class livro

--- src/br/com/edusys/firstrun.java
+++ src/br/com/edusys/firstrun.java
@@ -7,6 +7,43 @@ public class firstrun {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Problema 1", 300);
+        System.out.println(livro1);
+        System.out.println("etette");
     }
 
+}
+
+class Livro {
+    private String nomel;
+    private Integer numPaginas;
+
+    public Livro(String nomel, Integer numPaginas) {
+        this.nomel = nomel;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNomel() {
+        return nomel;
+    }
+
+    public void setNomel(String nomel) {
+        this.nomel = nomel;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Livro{" +
+                "nomel='" + nomel + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
